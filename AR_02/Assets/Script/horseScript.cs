a3933df95097e7582ce51cb8fde16b683cefef30
vfa-huyenttk <vfa.huyenttk@gmail.com>
2017-04-02 22:15:26 +0700

    Commit source AR demo

---
+++ AR_02/Assets/Script/horseScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horseScript : MonoBehaviour {
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void run(){
+		anim.SetBool ("isWork", false);
+		anim.SetBool("isRun", true);
+	}
+
+	public void walk(){
+		anim.SetBool ("isRun", false);
+		anim.SetBool("isWork", true);
+	}
+}
